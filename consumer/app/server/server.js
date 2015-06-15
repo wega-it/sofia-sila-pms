40c919ae36b2fd25f9642f8450f1f0553027e60b
wega-jh <juergen.hiss@wega-it.com>
2015-06-15 13:31:34 +0200

    Added dummy device

--- consumer/app/server/server.js
+++ consumer/app/server/server.js
@@ -34,7 +34,24 @@ Meteor.startup(function() {
 			process.env[variableName] = Meteor.settings.env[variableName];
 		}
 	}
+    
+    //Add dummy device
+    
+    if (!Devices.findOne({_id: "0000000000"})) {
+        Devices.insert({
+            "_id": "0000000000",
+            "name": "<====== Please select Device =====>",
+		    "silaInterfaceVersion" : "0.0.0",
+			"silaDeviceClassId": "X", 
+			"silaDeviceClassVersion": "0.0.0",
+            "deviceManufacturer" : "UNKNOWN",
+            "note" : "Dummy Device",
+            "deviceSerialNumber" : "00000000",
+            "deviceFirmwareVersion": "0.0.0"
+        });
+    }
 
+    
 	//Add initial documents to the database if empty
 	if(!CommonCommandSet.find().count()) {
 		CommonCommandSet.insert({"deviceClassVersion":0.97,"name":"Abort","parameters":"-","returnValues":"","0":"M","1":"M","2":"M","3":"M","4":"M","5":"M","6":"M","7":"M","8":"M","9":"M","10":"M","11":"M","12":"M","13":"M","14":"M","15":"M","16":"M","17":"M","18":"M","19":"M","20":"M","21":"M","22":"M","23":"M","24":"M","25":"M","26":"M","27":"M","28":"M","29":"M","30":"M","31":"M","901":"M","902":"M","903":"M","1000":"M"});
